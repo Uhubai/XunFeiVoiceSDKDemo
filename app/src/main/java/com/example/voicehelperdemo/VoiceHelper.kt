9c78adc19610bdfede41a19a4faa3f7246e1caea
Uhubai <82660209+Uhubai@users.noreply.github.com>
2024-04-01 15:57:55 +0800

    修复commander 不能使用的问题

--- app/src/main/java/com/example/voicehelperdemo/VoiceHelper.kt
+++ app/src/main/java/com/example/voicehelperdemo/VoiceHelper.kt
@@ -16,10 +16,12 @@ import java.util.concurrent.atomic.AtomicReference
 
 object VoiceHelper {
     private const val TAG = "VoiceHelper"
+    private var context = WeakReference<Context>(null)
     private var currentAction = AtomicReference("")
     private val aiHelper: AiHelper = AiHelper.getInst()
     private var aiHandle: AiHandle? = null
     private var aiResultListener: AiResultListener? = null
+    private val WORK_DIR = "/sdcard/iflytekAikit"
 
     private val coreListener = AuthListener { type, code ->
         Log.i(TAG, "core listener code:$code")
@@ -27,6 +29,17 @@ object VoiceHelper {
             ErrType.AUTH -> Log.i(TAG, "SDK状态：授权结果码$code")
             ErrType.HTTP -> Log.i(TAG, "SDK状态：HTTP认证结果$code")
             else -> Log.i(TAG, "SDK状态：其他错误")
+        }
+        if (currentAction.get().equals(context.get()!!.resources.getString(R.string.ability_commander))){
+            val engineBuilder = AiRequest.builder()
+            //解码类型 fsa:命令词, wfst:wfst解码, wfst_fsa:混合解 是 码
+            engineBuilder.param("decNetType", "fsa")
+            //fsa惩罚分数 最小值:0, 最大值:10
+            engineBuilder.param("punishCoefficient", 0.0)
+            //选择加载wfst资源 0中文，1英文
+            engineBuilder.param("wfst_addType", 0)
+            val ret = AiHelper.getInst().engineInit(currentAction.get(),engineBuilder.build())
+            Log.i(TAG, "engineInit: $ret")
         }
     }
     private val aiRespListener = object : AiListener {
@@ -55,6 +68,7 @@ object VoiceHelper {
     }
 
     fun initSDK(context: Context, ability: String) {
+        this.context = WeakReference(context)
         val params = Params.builder()
             .appId(context.resources.getString(R.string.app_id))
             .apiKey(context.resources.getString(R.string.api_key))
@@ -73,10 +87,37 @@ object VoiceHelper {
     }
 
     fun startAction(aiRequest: AiRequest.Builder) {
+        loadCommanderResource()
         aiHandle = AiHelper.getInst().start(currentAction.get(), aiRequest.build(), null)
         if (!aiHandle!!.isSuccess) {
             Log.e(TAG, "ERROR::START | handle code:" + aiHandle!!.code)
             return
+        }
+    }
+
+    private fun loadCommanderResource() {
+        if (context.get()!!.resources.getString(R.string.ability_commander).equals(currentAction.get())) {
+            val customBuilder = AiRequest.builder()
+            //设置FSA语法资源
+            val fsaDir = "${WORK_DIR}/esr/${if (0 == 0) "cn_fsa" else "en_fsa"}"
+            for (withIndex in EsrFsaEnum.entries.withIndex()) {
+                customBuilder.customText(
+                    "FSA",
+                    "${fsaDir}/${withIndex.value.path}",
+                    withIndex.index
+                )
+            }
+            var ret: Int = -1
+            ret = AiHelper.getInst().loadData(currentAction.get(), customBuilder.build())
+            Log.i(TAG, "loadData: $ret")
+            //指定加载设置的FSA语法文件，这里array中的index需要在上面设置的FSA语法文件中的index对应上
+            //这里暂时只指定加载index == 0 和 index == 1的， index= 2暂时不加载
+            ret = AiHelper.getInst().specifyDataSet(
+                currentAction.get(),
+                "FSA",
+                intArrayOf(0, 1, 3)
+            )
+            Log.i(TAG, "specifyDataSet: $ret")
         }
     }
 
@@ -140,4 +181,11 @@ object VoiceHelper {
     interface AiResultListener {
         fun onResult(p0: Int, outputData: AiResponse, p2: Any?)
     }
+}
+
+enum class EsrFsaEnum(val path: String) {
+    CALL("Call.txt"),
+    ALBUM("Album.txt"),
+    VIDEO("Video.txt"),
+    APP("App.txt")
 }
